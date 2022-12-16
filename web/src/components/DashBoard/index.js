33f67f84c21fafad01d5f3907c9714356df00f7c
Igor <ig.vp@hotmail.com>
2022-12-16 01:49:04 -0300

    first chart done

--- web/src/components/DashBoard/index.js
+++ web/src/components/DashBoard/index.js
@@ -9,23 +9,90 @@ import {
   SelectItems,
 } from "./styled";
 import { useForm } from "react-hook-form";
-
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const Dashboard = () => {
+  const [dataBrt, setDataBRT] = useState([]);
+  const [dataToShow, setDataToShow] = useState([]);
+  const [totalVendas, setTotalVendas] = useState(0.0);
+  useEffect(() => {
+    axios.get("http://localhost:8000/estacao/anos").then(({ data }) => {
+      const newData = [];
+      data.forEach((item) =>
+        newData.push({
+          ano: item.ano,
+          vendas: parseFloat(item.totalVendas),
+          vendasShow: parseFloat(item.totalVendas / 1000000),
+        })
+      );
+
+      setDataBRT(newData);
+      setDataToShow(newData);
+    });
+  }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/estacao/vendasValores")
+      .then(({ data }) => {
+        let Total = 0;
+        dataToShow.forEach((item) => {
+          const found = data.find((e) => e.ano == item.ano);
+          Total += parseFloat(found.tarifa) * item.vendas;
+        });
+        setTotalVendas(Math.round(Total / 1000000, 2));
+      });
+  }, [dataToShow]);
+  const HandleSelect = (e) => {
+    console.log(e);
+    if (e == "todos") {
+      setDataToShow(dataBrt);
+    } else {
+      setDataToShow(dataBrt.filter((item) => item.ano == e));
+    }
+  };
   return (
     <DashboardContainer>
       <TotalSalesContainer>
         <TotalSalesHeader>
           <TotalSalesHeaderTitleContainer>
-            <TotalSalesHeaderTitle>R$:1.5M</TotalSalesHeaderTitle>
+            <TotalSalesHeaderTitle>R$:{totalVendas}M</TotalSalesHeaderTitle>
           </TotalSalesHeaderTitleContainer>
           <TotalSalesHeaderTitleContainer2>
             <form>
-              <Select>
+              <Select onChange={(e) => HandleSelect(e.target.value)}>
                 <option value="todos">Todos</option>
+                {dataBrt.map((item) => (
+                  <option value={item.ano} key={item.ano}>
+                    {item.ano}
+                  </option>
+                ))}
               </Select>
             </form>
           </TotalSalesHeaderTitleContainer2>
         </TotalSalesHeader>
+        <ResponsiveContainer width="95%" height="75%">
+          <LineChart width={300} height={100} data={dataToShow}>
+            <Line
+              type="monotone"
+              dataKey="vendasShow"
+              stroke="#5C53BD"
+              strokeWidth={5}
+            />
+            <XAxis dataKey="ano" />
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
       </TotalSalesContainer>
     </DashboardContainer>
   );
--- web/src/components/DashBoard/styled.js
+++ web/src/components/DashBoard/styled.js
@@ -10,7 +10,6 @@ export const TotalSalesContainer = styled.div`
   width: 50%;
   height: 50%;
   background-color: ${({ theme }) => theme.colors.backgroundCards};
-  background-color: red;
   border-radius: 32px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);

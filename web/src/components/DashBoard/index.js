a6ed6df15e1fd0f98e19c9280bd4afbaa0a42fed
Igor <ig.vp@hotmail.com>
2022-12-16 23:15:17 -0300

    Station list skeleton done

--- web/src/components/DashBoard/index.js
+++ web/src/components/DashBoard/index.js
@@ -4,21 +4,11 @@ import {
   TotalSalesHeader,
   TotalSalesHeaderTitleContainer,
   TotalSalesHeaderTitle,
-  Select,
   TotalSalesHeaderTitleContainer2,
-  SelectItems,
+  TotalSalesListContainer,
+  TotalSalesListHeader,
 } from "./styled";
-import { useForm } from "react-hook-form";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from "recharts";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const Dashboard = () => {
@@ -49,7 +39,7 @@ const Dashboard = () => {
           const found = data.find((e) => e.ano == item.ano);
           Total += parseFloat(found.tarifa) * item.vendas;
         });
-        setTotalVendas(Math.round(Total / 1000000, 2));
+        setTotalVendas((Total / 1000000000).toFixed(2));
       });
   }, [dataToShow]);
   const HandleSelect = (e) => {
@@ -65,19 +55,10 @@ const Dashboard = () => {
       <TotalSalesContainer>
         <TotalSalesHeader>
           <TotalSalesHeaderTitleContainer>
-            <TotalSalesHeaderTitle>R$:{totalVendas}M</TotalSalesHeaderTitle>
+            <TotalSalesHeaderTitle>R$:{totalVendas}B</TotalSalesHeaderTitle>
           </TotalSalesHeaderTitleContainer>
           <TotalSalesHeaderTitleContainer2>
-            <form>
-              <Select onChange={(e) => HandleSelect(e.target.value)}>
-                <option value="todos">Todos</option>
-                {dataBrt.map((item) => (
-                  <option value={item.ano} key={item.ano}>
-                    {item.ano}
-                  </option>
-                ))}
-              </Select>
-            </form>
+            Vendas por ano
           </TotalSalesHeaderTitleContainer2>
         </TotalSalesHeader>
         <ResponsiveContainer width="95%" height="75%">
@@ -87,13 +68,17 @@ const Dashboard = () => {
               dataKey="vendasShow"
               stroke="#5C53BD"
               strokeWidth={5}
+              dot={false}
             />
             <XAxis dataKey="ano" />
-            <YAxis />
+
             <Tooltip />
           </LineChart>
         </ResponsiveContainer>
       </TotalSalesContainer>
+      <TotalSalesListContainer>
+        <TotalSalesListHeader></TotalSalesListHeader>
+      </TotalSalesListContainer>
     </DashboardContainer>
   );
 };
--- web/src/components/DashBoard/styled.js
+++ web/src/components/DashBoard/styled.js
@@ -4,6 +4,9 @@ export const DashboardContainer = styled.div`
   width: 100%;
   height: 100%;
   padding: 20px;
+  gap: 200px;
+  display: flex;
+  justify-content: center;
 `;
 
 export const TotalSalesContainer = styled.div`
@@ -49,20 +52,26 @@ export const TotalSalesHeaderTitleContainer2 = styled.div`
   border-radius: 32px;
   display: flex;
   flex-direction: row-reverse;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
-export const Select = styled.select`
-  margin-top: 5px;
-  border: none;
+export const TotalSalesListContainer = styled.div`
+  width: 300px;
+  height: 50%;
+  border-radius: 32px;
   background-color: ${({ theme }) => theme.colors.backgroundCards};
-  cursor: pointer;
-  font-family: "Poppins", sans-serif;
-  font-size: 16px;
-  color: "#FFF";
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 3.5px 7px rgba(0, 0, 0, 0.25), 0 3px 4px rgba(0, 0, 0, 0.22);
+  }
 `;
 
-export const SelectItems = styled.p`
-  font-family: "Poppins", sans-serif;
-  font-size: 16px;
-  color: "#FFF";
+export const TotalSalesListHeader = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: ${({ theme }) => theme.colors.backgroundCards};
+  border-radius: 32px 32px 0 0;
+  border-bottom: 0.7px solid rgba(0, 0, 0, 0.25);
 `;
